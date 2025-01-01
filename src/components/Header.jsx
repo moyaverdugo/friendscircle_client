67a0f0446b60b5857e3a0c2efe3fe8cca88a747f
Jorge Moya <107212424+moyaverdugo@users.noreply.github.com>
2024-12-31 18:08:26 -0800

    Initialization of Friends Circle Client

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <section className="header">
+      <div className="header__banner"></div>
+      <div className="header__bottom">
+        <Link className="header__logo" to="/">
+          BrainFeed
+        </Link>
+        <nav className="header__nav">
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? 'header__nav-link header__nav-link--active'
+                : 'header__nav-link'
+            }
+            to="/"
+            exact="true"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? 'header__nav-link header__nav-link--active'
+                : 'header__nav-link'
+            }
+            to="/profile"
+            exact="true"
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            className={(navData) =>
+              navData.isActive
+                ? 'header__nav-link header__nav-link--active'
+                : 'header__nav-link'
+            }
+            to="/posts"
+            exact="true"
+          >
+            Posts
+          </NavLink>
+        </nav>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
